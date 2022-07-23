f462462b95f29ab9a23749b8ac0bdf9c6c25c823
phoenix <betafoenix@gmail.com>
2022-07-23 18:35:43 +0300

    add action area

--- components/Photo/Photo.tsx
+++ components/Photo/Photo.tsx
@@ -1,10 +1,8 @@
 import { FC, memo } from 'react'
 import { IPhoto } from '@/types'
 
-import { Typography, Grid, Card, CardContent, CardMedia } from '@mui/material'
-// import { usePhoto } from '@/hooks'
+import { Typography, Grid, Card, CardContent, CardMedia, CardActionArea } from '@mui/material'
 
-import { Comment } from '@/components'
 import Image from 'next/image'
 
 export interface IPhotoProps {
@@ -16,14 +14,16 @@ export const Photo: FC<IPhotoProps> = memo(
     return (
       <Grid item sx={{width: '100%'}}>
         <Card>
-          <CardMedia sx={{ position: 'relative', width: '100%', height: '140px' }}>
-            <Image src={photo.thumbnailUrl} layout='fill' objectFit="cover" objectPosition='center' alt={photo.title} unoptimized />
-          </CardMedia>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography component="h2" variant="h5">
-              {photo.title}
-            </Typography>
-          </CardContent>
+          <CardActionArea>
+            <CardMedia sx={{ position: 'relative', width: '100%', height: '140px' }}>
+              <Image src={photo.thumbnailUrl} layout='fill' objectFit="cover" objectPosition='center' alt={photo.title} unoptimized />
+            </CardMedia>
+            <CardContent sx={{ flex: 1 }}>
+              <Typography component="h2" variant="h5">
+                {photo.title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
       </Grid>
     )
